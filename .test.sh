1d2f30376227397e384574ea0823ec249e898f87
Diego Hernandes <diego@hernandev.com>
2016-09-24 18:07:18 -0300

    forcing phpdbg for coverage reports

--- .test.sh
+++ .test.sh
@@ -7,7 +7,7 @@ cp .env.testing .env
 php artisan key:generate
 
 # Run PHPUnit Tests
-php vendor/bin/phpunit --colors=never --coverage-text
+phpdbg -qrr vendor/bin/phpunit --colors=never --coverage-text
 
 # Send Coverage Reports
 bash <(curl -s https://codecov.io/bash) -f coverage.xml -C$TRAVIS_COMMIT -B$TRAVIS_BRANCH -b$TRAVIS_BUILD_NUMBER -t$CODECOV_TOKEN
