13ef6d7fe4e81fcdfcb7de48ca26b7b9e618e3d2
Diego Hernandes <diego@hernandev.com>
2016-09-24 23:01:32 -0300

    Postcasts Migrations

---
+++ app/Domains/Podcasts/Database/Migrations/AddPlayedAndDownloadTimesToLessonsTable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Codecasts\Domains\Podcasts\Database\Migrations;
+
+use Codecasts\Support\Domain\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddPlayedAndDownloadTimesToLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        $this->schema->table('podcasts', function (Blueprint $table) {
+            $table->unsignedInteger('times_downloaded')->default(0)->after('published_at');
+            $table->unsignedInteger('times_played')->default(0)->after('published_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        $this->schema->table('podcasts', function (Blueprint $table) {
+            $table->dropColumn('times_downloaded');
+            $table->dropColumn('times_played');
+        });
+    }
+}
