f84014d73835dca7bf8b25e60044f127c28bf555
Diego Hernandes <diego@hernandev.com>
2016-09-24 22:50:14 -0300

    Lesson and Track repositories

---
+++ app/Domains/Lessons/Repositories/LessonRepository.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Codecasts\Domains\Lessons\Repositories;
+
+use Artesaos\Warehouse\AbstractCrudRepository;
+use Artesaos\Warehouse\Traits\ImplementsFractal;
+use Codecasts\Domains\Lessons\Lesson;
+use Codecasts\Domains\Lessons\Transformers\LessonTransformer;
+use Codecasts\Domains\Lessons\Contracts\LessonRepository as LessonRepositoryContract;
+use Codecasts\Support\ElasticSearch\Contracts\SearchRepository;
+use Codecasts\Support\ElasticSearch\SearchableTrait;
+
+class LessonRepository extends AbstractCrudRepository implements LessonRepositoryContract, SearchRepository
+{
+    use ImplementsFractal, SearchableTrait;
+
+    protected $modelClass = Lesson::class;
+
+    protected $transformerClass = LessonTransformer::class;
+
+    public function getAll($take = 15, $paginate = true)
+    {
+        $query = $this->newQuery();
+        $query->orderBy('published_at', 'desc');
+
+        return $this->doQuery($query, $take, $paginate);
+    }
+
+    public function getVisible($take = 9, $paginate = true)
+    {
+        $query = $this->newQuery();
+        $query->where('visible', true);
+        $query->orderBy('published_at', 'desc');
+
+        return $this->doQuery($query, $take, $paginate);
+    }
+
+    public function findBySlug($slug, $fail = false)
+    {
+        $query = $this->newQuery()->where('slug', $slug);
+
+        if ($fail) {
+            return $query->firstOrFail();
+        } else {
+            return $query->first();
+        }
+    }
+
+    public function findByEncryptedId($encrypted_id)
+    {
+        $id = app('encrypter')->decrypt($encrypted_id);
+
+        $query = $this->newQuery()->where('id', $id);
+
+        return $query->first();
+    }
+
+    public function totalPublishedTime()
+    {
+        $sum = $this->newQuery()
+            ->where('visible', true)->sum('length');
+
+        return $sum;
+    }
+
+    public function visibleCount()
+    {
+        $count = $this->newQuery()
+            ->where('visible', true)->count();
+
+        return $count;
+    }
+}
---
+++ app/Domains/Lessons/Repositories/TrackRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Codecasts\Domains\Lessons\Repositories;
+
+use Artesaos\Warehouse\AbstractCrudRepository;
+use Artesaos\Warehouse\Traits\ImplementsFractal;
+use Codecasts\Domains\Lessons\Track;
+use Codecasts\Domains\Lessons\Transformers\TrackTransformer;
+use Codecasts\Domains\Lessons\Contracts\TrackRepository as TrackRepositoryContract;
+
+class TrackRepository extends AbstractCrudRepository implements TrackRepositoryContract
+{
+    use ImplementsFractal;
+
+    protected $modelClass = Track::class;
+
+    protected $transformerClass = TrackTransformer::class;
+
+    public function getAll($take = 15, $paginate = true)
+    {
+        $query = $this->newQuery()->orderBy('id', 'desc');
+
+        return $this->doQuery($query, $take, $paginate);
+    }
+
+    /**
+     * @param string $slug
+     * @param bool   $fail
+     *
+     * @return Track|null
+     */
+    public function findBySlug($slug, $fail = false)
+    {
+        $query = $this->newQuery()->where('slug', $slug);
+
+        if ($fail) {
+            return $query->firstOrFail();
+        } else {
+            return $query->first();
+        }
+    }
+
+    public function getVisible($take = 9, $paginate = true)
+    {
+        $query = $this->newQuery();
+        $query->leftJoin('lessons', 'lessons.track_id', '=', 'tracks.id');
+        $query->select('tracks.*');
+        $query->groupBy('tracks.id');
+        $query->orderByRaw('max(lessons.published_at) desc');
+
+        return $this->doQuery($query, $take, $paginate);
+    }
+}
