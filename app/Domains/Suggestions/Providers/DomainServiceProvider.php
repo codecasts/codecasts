b2cf06323a0ba811618707af1dae1c19fe16c385
Diego Hernandes <diego@hernandev.com>
2016-09-24 23:35:17 -0300

    finishing suggestions domain

---
+++ app/Domains/Suggestions/Providers/DomainServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Codecasts\Domains\Suggestions\Providers;
+
+use Codecasts\Domains\Suggestions\Database\Migrations\CreateSuggestionsTable;
+use Codecasts\Domains\Suggestions\Database\Migrations\CreateSuggestionsVotesTable;
+use Codecasts\Domains\Suggestions\Contracts\SuggestionRepository as SuggestionRepositoryContract;
+use Codecasts\Domains\Suggestions\Repositories\SuggestionRepository;
+use Codecasts\Support\Domain\ServiceProvider;
+
+class DomainServiceProvider extends ServiceProvider
+{
+    protected $alias = 'suggestions';
+
+    protected $hasTranslations = true;
+
+    protected $bindings = [
+        SuggestionRepositoryContract::class => SuggestionRepository::class,
+    ];
+
+    protected $migrations = [
+        CreateSuggestionsTable::class,
+        CreateSuggestionsVotesTable::class,
+    ];
+}
