f86f1aa9754fc7db8cd48571f63fb446f1abebb0
Diego Hernandes <diego@hernandev.com>
2016-09-24 22:52:22 -0300

    Lessons domain resources (translations)

---
+++ app/Domains/Lessons/Resources/Lang/en/time.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'sec' => 'sec',
+    'seconds' => 'seconds',
+    'min' => 'min',
+    'minutes' => 'minutes',
+];
---
+++ app/Domains/Lessons/Resources/Lang/pt_BR/time.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'sec' => 'seg',
+    'seconds' => 'segundos',
+    'min' => 'min',
+    'minutes' => 'minutos',
+];
