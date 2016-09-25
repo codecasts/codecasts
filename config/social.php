3cf7e7dfa018ebd2f369f801f39fd1a59caab720
Diego Hernandes <diego@hernandev.com>
2016-09-24 22:04:15 -0300

    config file for social parser bindings

---
+++ config/social.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'parsers' => [
+        'facebook' => \Codecasts\Domains\Users\Parsers\FacebookParser::class,
+        'github' => \Codecasts\Domains\Users\Parsers\GithubParser::class,
+        'google' => \Codecasts\Domains\Users\Parsers\GoogleParser::class,
+    ]
+
+];
