8a6c4a81647426b401b417b9dd0d3b2d8552f99b
Diego Hernandes <diego@hernandev.com>
2016-09-26 12:29:28 -0300

    unit tests for Disqus comments

---
+++ tests/unit/Support/Comments/DisqusTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Codecasts\Support\Comments;
+
+use Illuminate\Contracts\View\Factory;
+use Mockery as m;
+
+/**
+ * Test Disqus comments
+ */
+class DisqusTest extends \TestCase
+{
+    public function test_disqus_view_is_called_correctly()
+    {
+        $viewFactory = m::mock(Factory::class)
+            ->shouldReceive('make')
+            ->once()
+            ->with('disqus', ['disqus_id' => 1, 'disqus_title' => 'foo'], [])
+            ->getMock();
+
+        $this->app->instance(Factory::class, $viewFactory);
+
+        Disqus::display(1, 'foo');
+    }
+}
