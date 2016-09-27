66cd99c39bdc53617effd21d8ce76a5eb60a563e
Diego Hernandes <diego@hernandev.com>
2016-09-27 00:07:37 -0300

    slack notifications test

---
+++ tests/unit/Support/Notifications/SlackTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Codecasts\Support\Notifications;
+
+use Mockery as m;
+use Maknz\Slack\Client as SlackClient;
+
+/**
+ * Class SlackTest.
+ */
+class SlackTest extends \TestCase
+{
+    public function test_send_notifications()
+    {
+        $slackClient = m::mock(SlackClient::class)
+            ->shouldReceive('send')
+            ->with('foo')
+            ->getMock();
+
+        $this->app->instance('maknz.slack', $slackClient);
+
+        $slack = new Slack();
+
+        $slack->notify('foo');
+    }
+
+    public function test_send_notifications_with_attached_fields()
+    {
+        $clientAttachMock = m::mock(SlackClient::class)
+            ->shouldReceive('send')
+            ->once()
+            ->with('foo')->getMock();
+
+        $slackClient = m::mock(SlackClient::class)
+            ->shouldReceive('attach')
+            ->with(['fields' => [
+                ['title' => 'field1', 'value' => 'value1'],
+                ['title' => 'field2', 'value' => 'value2'],
+            ]])
+            ->once()
+            ->andReturn($clientAttachMock)
+            ->getMock();
+
+        $this->app->instance('maknz.slack', $slackClient);
+
+        $slack = new Slack();
+
+        $slack->notify('foo', [
+            'field1' => 'value1',
+            'field2' => 'value2',
+        ]);
+    }
+}
