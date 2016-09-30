bbe23768253ca736d47d5d5f4dc10695038e4619
Diego Hernandes <diego@hernandev.com>
2016-09-29 23:52:05 -0300

    elastic search indexer

---
+++ app/Support/ElasticSearch/Indexer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Codecasts\Support\ElasticSearch;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Class Indexer.
+ */
+class Indexer
+{
+    /**
+     * @var Collection
+     */
+    protected $items;
+
+    /**
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Indexer constructor.
+     * @param Collection $items
+     * @param string $type
+     */
+    public function __construct(Collection $items, $type)
+    {
+        $this->items = $items;
+
+        $this->type = $type;
+    }
+
+    public function index()
+    {
+        $elastic = app('elastic');
+
+        $this->items->each(function($item) use ($elastic) {
+            $item->track_title = $item->track ? $item->track->title : '';
+
+            $elastic->index([
+                'index' => 'codecasts',
+                'type' => $this->type,
+                'id' => $item->id,
+                'body' => $item->toArray(),
+            ]);
+        });
+    }
+}
