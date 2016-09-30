4f9d2e7e10e69bb1a92e8ec8557075d3bab8ccd4
Diego Hernandes <diego@hernandev.com>
2016-09-30 00:59:32 -0300

    starting translating validation messages (be welcome to finish it)

---
+++ resources/lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senhas precisam ter pelo menos 6 caracteres e a confirmação deve ser igual.',
+    'reset'    => 'Sua senha foi resetada!',
+    'sent'     => 'Mandamos um email com um link para recuperar sua senha!',
+    'token'    => 'O Token é inválido.',
+    'user'     => "Não achamos ninguem com esse email, confere ai 'parça'.",
+
+];
